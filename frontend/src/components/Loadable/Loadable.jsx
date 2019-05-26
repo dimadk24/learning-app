b34d6d611e4c24c5b5353524cb05fe6bc26e1fde
DimaDK <DimaKrutDK@ya.ru>
2019-05-26 16:42:34 +0300

    fix: make Loadable work fine with class components
    i.e. not only function-based components

--- frontend/src/components/Loadable/Loadable.jsx
+++ frontend/src/components/Loadable/Loadable.jsx
@@ -4,11 +4,13 @@ import RealLoadable from 'react-loadable'
 import Loader from './Loader'
 
 function Loadable(props) {
-  const { render, loader, ...other } = props
+  const { render: Component, loader, ...other } = props
   const Content = RealLoadable({
-    loader,
-    loading: Loader,
-    render,
+    loader, // function which loads some required stuff and returns Promise
+    loading: Loader, // component to show while loading
+    render: (loadedContent, passedProps) => {
+      return <Component {...loadedContent} {...passedProps} />
+    },
     timeout: 10000,
   })
   return <Content {...other} />
