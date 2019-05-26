963511daf6a247588fa5b9a9d4ff42fe3f5900c8
DimaDK <DimaKrutDK@ya.ru>
2019-05-26 16:44:37 +0300

    feat: show score for completed tests

--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Question from '../../components/Question/Question'
 
-// will be used later
-// eslint-disable-next-line no-unused-vars
-const TestPage = ({ id, name, questions, completed }) => {
-  return (
-    <div className={completed ? 'disabled' : ''}>
-      <span>Тест «{name}»</span>
-      {questions.map((question) => (
-        <Question {...question} key={question.id} testIsFinished={completed} />
-      ))}
-    </div>
-  )
+class TestPage extends Component {
+  getPercentScore() {
+    const { score } = this.props
+    const percentScore = score * 100
+    return `${percentScore}%`
+  }
+
+  render() {
+    // will be used later
+    // eslint-disable-next-line no-unused-vars
+    const { id, name, questions, completed } = this.props
+    return (
+      <div className={completed ? 'disabled' : ''}>
+        <span>Тест «{name}»</span>
+        {questions.map((question) => (
+          <Question
+            {...question}
+            key={question.id}
+            testIsFinished={completed}
+          />
+        ))}
+        {completed && <span>Оценка: {this.getPercentScore()}</span>}
+      </div>
+    )
+  }
 }
 
 TestPage.propTypes = {
@@ -21,6 +35,11 @@ TestPage.propTypes = {
   // clear propTypes for question are defined in Question component
   questions: PropTypes.arrayOf(PropTypes.object).isRequired,
   completed: PropTypes.bool.isRequired,
+  score: PropTypes.number,
+}
+
+TestPage.defaultProps = {
+  score: 0,
 }
 
 export default TestPage
