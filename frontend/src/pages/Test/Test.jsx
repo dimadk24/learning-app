032b3cc5b23b8a484e0ccf58f624a1b6f8579907
DimaDK <DimaKrutDK@ya.ru>
2019-05-26 18:30:28 +0300

    feat: rendering available for test time

--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -1,21 +1,38 @@
+import moment from 'moment'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Question from '../../components/Question/Question'
 
 class TestPage extends Component {
+  constructor(props) {
+    super(props)
+    const { time } = props
+    this.state = {
+      availableTime: time,
+    }
+  }
+
   componentDidMount() {
     const { completed, time } = this.props
     if (!completed && time) {
-      this.timerId = setTimeout(() => {
+      this.sendResultsTimerId = setTimeout(() => {
         // send test result here
         // onSend clearTimeout
       }, time * 1000)
+      this.updateClockTimerId = setInterval(() => {
+        this.setState(({ availableTime }) => {
+          return { availableTime: availableTime - 1 }
+        })
+      }, 1000)
     }
   }
 
   componentWillUnmount() {
-    if (this.timerId) {
-      clearTimeout(this.timerId)
+    if (this.sendResultsTimerId) {
+      clearTimeout(this.sendResultsTimerId)
+    }
+    if (this.updateClockTimerId) {
+      clearInterval(this.updateClockTimerId)
     }
   }
 
@@ -25,6 +42,11 @@ class TestPage extends Component {
     return `${percentScore}%`
   }
 
+  renderAvailableTime() {
+    const { availableTime } = this.state
+    return moment.unix(availableTime).format('mm:ss')
+  }
+
   render() {
     // will be used later
     // eslint-disable-next-line no-unused-vars
@@ -32,6 +54,9 @@ class TestPage extends Component {
     return (
       <div className={completed ? 'disabled' : ''}>
         <span>Тест «{name}»</span>
+        {!completed && (
+          <span>Осталось времени: {this.renderAvailableTime()}</span>
+        )}
         {questions.map((question) => (
           <Question
             {...question}
@@ -52,7 +77,7 @@ TestPage.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.object).isRequired,
   completed: PropTypes.bool.isRequired,
   score: PropTypes.number,
-  time: PropTypes.number,
+  time: PropTypes.number, // time to complete test in seconds
 }
 
 TestPage.defaultProps = {
