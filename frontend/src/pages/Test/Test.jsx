7d302d2e016fe9708081dd73c5aee551d1ee72b2
DimaDK <DimaKrutDK@ya.ru>
2019-06-02 10:55:20 +0300

    chore: revert add of time to complete the test
    There's no time to focus on such minor features
    This commit reverts:
    66ca9477
    6cb0e7ca
    032b3cc5

--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -1,50 +1,12 @@
-import moment from 'moment'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Question from '../../components/Question/Question'
 
 class TestPage extends Component {
-  constructor(props) {
-    super(props)
-    const { time } = props
-    this.state = {
-      availableTime: time,
-    }
-  }
-
-  componentDidMount() {
-    const { completed, time } = this.props
-    if (!completed && time) {
-      this.sendResultsTimerId = setTimeout(() => {
-        // send test result here
-        // onSend clearTimeout
-      }, time * 1000)
-      this.updateClockTimerId = setInterval(() => {
-        this.setState(({ availableTime }) => {
-          return { availableTime: availableTime - 1 }
-        })
-      }, 1000)
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.sendResultsTimerId) {
-      clearTimeout(this.sendResultsTimerId)
-    }
-    if (this.updateClockTimerId) {
-      clearInterval(this.updateClockTimerId)
-    }
-  }
-
   getPercentScore() {
     const { score } = this.props
     const percentScore = score * 100
     return `${percentScore}%`
-  }
-
-  renderAvailableTime() {
-    const { availableTime } = this.state
-    return moment.unix(availableTime).format('mm:ss')
   }
 
   render() {
@@ -54,9 +16,6 @@ class TestPage extends Component {
     return (
       <div className={completed ? 'disabled' : ''}>
         <span>Тест «{name}»</span>
-        {!completed && (
-          <span>Осталось времени: {this.renderAvailableTime()}</span>
-        )}
         {questions.map((question) => (
           <Question
             {...question}
@@ -77,12 +36,10 @@ TestPage.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.object).isRequired,
   completed: PropTypes.bool.isRequired,
   score: PropTypes.number,
-  time: PropTypes.number, // time to complete test in seconds
 }
 
 TestPage.defaultProps = {
   score: 0,
-  time: 0,
 }
 
 export default TestPage
